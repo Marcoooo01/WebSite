058f16a5473c5f849f39a66cea2058e268b0cbfc
Marcoooo01 <grossi.marco@iisgalvanimi.edu.it>
2020-01-14 19:29:43 +0000

    Files Changed

--- PlayerStatistics/routes/statistics.js
+++ PlayerStatistics/routes/statistics.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var unirest = require("unirest");
 const fs = require('fs');
+
+//PROVA CON VARIABILI A CASO
 let playerID = 242
 let season = "2019-2020"
+
 router.get('/', function(req, res, next) {
     var req = unirest("GET", "https://api-football-v1.p.rapidapi.com/v2/players/player/" + playerID + "/" + season);
 
@@ -15,10 +18,9 @@ router.get('/', function(req, res, next) {
     
     req.end(function (res) {
        if (res.error) throw new Error(res.error);
-    let data = JSON.stringify(res.body);
-    fs.writeFileSync('player.json', data);
-    console.log(res);
-});
+       let data = JSON.stringify(res.body);
+       fs.writeFileSync('player.json', data);
+    });
 
 });
 
