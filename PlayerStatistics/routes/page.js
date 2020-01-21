14d35414adf71e95df81755b0c3c04e34a023015
Marcoooo01 <grossi.marco@iisgalvanimi.edu.it>
2020-01-21 12:55:21 +0000

    Updates

--- PlayerStatistics/routes/page.js
+++ PlayerStatistics/routes/page.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+const player = require('../player.json');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    console.log("ciao");
+router.post('/', function(req, res, next) {
+    if (req.body.selector1 == 'a'){
+        res.render('all', {
+            player: player.api.players
+        })
+    }else{
+        res.render('season', {
+            player: player.api.players
+        })
+    }
 });
 
 module.exports = router;
